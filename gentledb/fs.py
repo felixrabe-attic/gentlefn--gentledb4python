3094077d8723de5cd2ff0db049f0c34a50e2647f
Felix Rabe <public@felixrabe.net>
2011-06-10 22:21:45 +0200

    FS implementation: Various changes

--- gentledb/fs.py
+++ gentledb/fs.py
@@ -21,7 +21,6 @@ from __future__ import absolute_import, print_function
 import glob
 from hashlib import sha256
 import os
-import tempfile
 
 from . import interfaces, utilities
 
@@ -66,17 +65,17 @@ class GentleDB(interfaces.GentleDB):
         return found
 
     def __add__(self, content):
-        content_id = sha256(content).hexdigest()
-        filename = self._get_content_filename(content_id, create_dir=True)
-        if not os.path.exists(filename):
-            with utilities.create_file_with_mode(filename, 0400) as f:
-                f.write(content)
+        f = self()
+        f.write(content)
+        content_id = f()
+        f.close()
         return content_id
 
     def __sub__(self, content_id):
         utilities.validate_identifier(content_id)
-        filename = self._get_content_filename(content_id, create_dir=False)
-        content = open(filename, "rb").read()
+        f = self(content_id)
+        content = f.read()
+        f.close()
         return content
 
     def __invert__(self):
@@ -84,12 +83,12 @@ class GentleDB(interfaces.GentleDB):
 
     def __setitem__(self, pointer_id, content_id):
         utilities.validate_identifier(pointer_id)
-        if content_id:
+        if content_id is not None:
             utilities.validate_identifier(content_id)
             filename = self._get_pointer_filename(pointer_id, create_dir=True)
             with utilities.create_file_with_mode(filename, 0600) as f:
                 f.write(content_id)
-        else:  # remove pointer
+        else:  # content_id is None, so remove the pointer
             filename = self._get_pointer_filename(pointer_id, create_dir=False)
             if os.path.exists(filename):
                 os.remove(filename)
@@ -127,9 +126,9 @@ class _OutFile(object):
         super(_OutFile, self).__init__()
         self.db = db
         self.hash_obj = sha256()
-        directory = os.path.join(self.db.directory, "tmp")
-        self.tmpfile_f, self.tmpfile_path = tempfile.mkstemp(dir=directory)
-        self.tmpfile = os.fdopen(self.tmpfile_f, "wb")
+        directory = self.db.tmp_dir
+        self.tmpfile_path = os.path.join(directory, utilities.random())
+        self.tmpfile = utilities.create_file_with_mode(self.tmpfile_path, 0600)
         self.is_open = True
 
     def write(self, data):
@@ -138,7 +137,7 @@ class _OutFile(object):
 
     def close(self):
         if not self.is_open:
-            raise utilities.GentleDBException("File already closed")
+            return
         self.tmpfile.close()
         self.is_open = False
         content_id = self()
@@ -166,6 +165,9 @@ class _InFile(object):
         super(_InFile, self).__init__()
         filename = db._get_content_filename(content_id, create_dir=False)
         self.content_file = open(filename, "rb")
+
+    def close(self):
+        self.content_file.close()
 
     def read(self, size=-1):
         return self.content_file.read(size)
